52fa188fe41de1e46c0e333d6b425aadc87f2555
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-14 19:36:11 -0700

    Update esm_step_3_15B_divide5_no_matrix_dirpath.sh

--- src/training/V3/esm_step_3_15B_divide5_no_matrix_dirpath.sh
+++ src/training/V3/esm_step_3_15B_divide5_no_matrix_dirpath.sh
@@ -160,7 +160,8 @@ python -u run.py \
   --no_token_type_embeddings \
   --no_position_embeddings \
   --use_rotary_position_embeddings \
-  --divide_classification_weight 5
+  --divide_classification_weight 5 \
+  --embedding_input_size 5120 \
 
 
 

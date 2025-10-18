1e0aad491f50635e6d4cb02c4531dcd18317c9ac
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-17 18:09:39 -0700

    Update run_transformer_step_1_037.sh

--- bash_scripts_nibi/step_1/transformer/run_transformer_step_1_037.sh
+++ bash_scripts_nibi/step_1/transformer/run_transformer_step_1_037.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=4-0
-#SBATCH --job-name=transformer-step-1-256-4096
-#SBATCH --output=output/transformer_step_1_256_4096_%j.out
-#SBATCH --err=output/transformer_step_1_256_4096_%j.err
+#SBATCH --job-name=transformer-step-1-037
+#SBATCH --output=output/transformer_step_1_037_%j.out
+#SBATCH --err=output/transformer_step_1_037_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat transformer_step_1_256_4096.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_1/transformer/output/transformer_step_1_256_4096_$SLURM_JOB_ID.txt
+cat transformer_step_1_256_4096.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_1/transformer/output/transformer_step_1_037_$SLURM_JOB_ID.txt
 ./transformer_step_1_256_4096.sh
 
 

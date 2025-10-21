85995bbccc7df8f0a8fa2663a18fd83612eedc24
sbc806 <sarahc19233@gmail.com>
2025-10-21 17:24:20 -0400

    Added scripts.

---
+++ bash_scripts_nibi/step_3/transformer/run_transformer_step_3_lr2e5_divide5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=64G
+#SBATCH --time=4-0
+#SBATCH --job-name=transformer-step-3-8-1-divide5
+#SBATCH --output=output/transformer_step_3_8_1_divide5_%j.out
+#SBATCH --err=output/transformer_step_3_8_1_divide5_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat transformer_step_3_8_1_divide5.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/transformer/output/transformer_step_3_8_1_divide5_$SLURM_JOB_ID.txt
+./transformer_step_3_8_1_divide5.sh
+
+
+deactivate

467401746a54e741298acfd0bba729f61c41588c
sbc806 <sarahc19233@gmail.com>
2025-10-21 17:49:45 -0400

    Adding files.

---
+++ bash_scripts_nibi/step_1/transformer/run_transformer_step_1_b16_g16.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=4-0
+#SBATCH --job-name=transformer-step-1-037-512
+#SBATCH --output=output/transformer_step_1_037_512_%j.out
+#SBATCH --err=output/transformer_step_1_037_512_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat transformer_step_1_4_1_hidden512.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_1/transformer/output/transformer_step_1_037_512_$SLURM_JOB_ID.txt
+./transformer_step_1_4_1_hidden512.sh
+
+
+deactivate

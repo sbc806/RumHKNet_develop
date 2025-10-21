2280b3c0b44c0d572c889beb1511684b357f169d
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-21 12:10:24 -0700

    Update esm_step_3_8M_divide5_no_matrix_dirpath.sh

--- src/training/V3/esm_step_3_8M_divide5_no_matrix_dirpath.sh
+++ src/training/V3/esm_step_3_8M_divide5_no_matrix_dirpath.sh
@@ -99,7 +99,7 @@ python -u run.py \
   --seq_subword \
   --codes_file ../subword/$tokenizer_dir/$codes_file \
   --label_filepath ../kinases_dataset/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/label.txt  \
-  --output_dir ../models_esm_step_3/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
+  --output_dir ../models_esm_step_3_8M/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --log_dir ../logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --tb_log_dir ../tb-logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --config_path ../config/$MODEL_TYPE/$CONFIG_NAME \

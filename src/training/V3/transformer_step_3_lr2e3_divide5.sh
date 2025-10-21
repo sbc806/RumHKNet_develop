917127d7aada43eeaddd5ceb802236030fcabf7a
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-21 14:33:24 -0700

    Update transformer_step_3_lr2e3_divide5.sh

--- src/training/V3/transformer_step_3_lr2e3_divide5.sh
+++ src/training/V3/transformer_step_3_lr2e3_divide5.sh
@@ -68,7 +68,7 @@ max_steps=-1
 ## batch size for one GPU
 batch_size=16
 ## 最大学习速率(peak learning rate)
-learning_rate=2e-4
+learning_rate=2e-3
 ## data loading buffer size
 buffer_size=4096
 ## tokenizer dir
@@ -99,7 +99,7 @@ python -u run_seq_only.py \
   --seq_subword \
   --codes_file ../subword/$tokenizer_dir/$codes_file \
   --label_filepath ../kinases_dataset/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/label.txt  \
-  --output_dir ../models/step_3/shallow_100/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
+  --output_dir ../models/step_3_lr2e3/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --log_dir ../logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --tb_log_dir ../tb-logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --config_path ../config/$MODEL_TYPE/$CONFIG_NAME \

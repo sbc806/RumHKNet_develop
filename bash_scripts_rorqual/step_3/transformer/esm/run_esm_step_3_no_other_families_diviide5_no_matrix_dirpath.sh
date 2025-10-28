42dd94d76be6e2f2b35f3b2035bdb23f92829874
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-27 18:11:16 -0700

    Update run_esm_step_3_no_other_families_diviide5_no_matrix_dirpath.sh

--- bash_scripts_rorqual/step_3/transformer/esm/run_esm_step_3_no_other_families_diviide5_no_matrix_dirpath.sh
+++ bash_scripts_rorqual/step_3/transformer/esm/run_esm_step_3_no_other_families_diviide5_no_matrix_dirpath.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=rrg-guanuofa
+#SBATCH --account=def-guanuofa
 #SBATCH --gpus=h100:1
 #SBATCH --mem=128G
 #SBATCH --time=7-0
@@ -13,12 +13,12 @@ module load scipy-stack
 module load gcc arrow/21.0.0
 
 
-cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../sbc806/RumHKNet/src/training/V3
-cat esm_step_3_no_other_families_divide5_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/esm/output/esm_step_3_no_other_families_divide5_no_matrix_dirpath_$SLURM_JOB_ID.txt
+cd ../sbc806_2/RumHKNet/src/training/V3
+cat esm_step_3_no_other_families_divide5_no_matrix_dirpath.sh > /home/schen123/links/projects/def-guanuofa/schen123/kinases/sbc806_2/RumHKNet/bash_scripts_nibi/step_3/esm/output/esm_step_3_no_other_families_divide5_no_matrix_dirpath_$SLURM_JOB_ID.txt
 ./esm_step_3_no_other_families_divide5_no_matrix_dirpath.sh
 
 

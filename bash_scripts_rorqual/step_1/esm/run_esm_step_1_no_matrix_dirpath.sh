3256f39d59de2388368a367d393c5b00fa247585
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-28 17:10:44 -0700

    Update run_esm_step_1_no_matrix_dirpath.sh

---
+++ bash_scripts_rorqual/step_1/esm/run_esm_step_1_no_matrix_dirpath.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus=h100:1
+#SBATCH --mem=256G
+#SBATCH --time=7-0
+#SBATCH --job-name=esm-step-1-no-matrix-dirpath
+#SBATCH --output=output/esm_step_1_no_matrix_dirpath_%j.out
+#SBATCH --err=output/esm_step_1_no_matrix_dirpath_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806_2/RumHKNet/src/training/V3
+cat esm_step_1_no_matrix_dirpath.sh > /home/schen123/links/projects/def-guanuofa/schen123/kinases/sbc806_2/RumHKNet/bash_scripts_rorqual/step_1/esm/output/esm_step_1_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./esm_step_1_no_matrix_dirpath.sh
+
+
+deactivate

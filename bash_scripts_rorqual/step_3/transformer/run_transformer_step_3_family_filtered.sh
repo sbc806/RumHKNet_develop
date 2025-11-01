35c3b5c188dae88d8e95fe5b51238a073272ec0b
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-10-31 22:45:59 -0700

    Create run_transformer_step_3_family_filtered.sh

---
+++ bash_scripts_rorqual/step_3/transformer/run_transformer_step_3_family_filtered.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=h100_3g.40gb:1
+#SBATCH --mem=32G
+#SBATCH --time=4-0
+#SBATCH --job-name=transformer-step-3-family-filtered
+#SBATCH --output=output/transformer_step_3_family_filtered_%j.out
+#SBATCH --err=output/transformer_step_3_family_filtered_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806_2/RumHKNet/src/training/V3
+cat transformer_step_3_family_filtered.sh > /home/schen123/links/projects/def-guanuofa/schen123/kinases/sbc806_2/RumHKNet/bash_scripts_rorqual/step_3/transformer/output/transformer_step_3_family_filtered_$SLURM_JOB_ID.txt
+./transformer_step_3_family_filtered.sh
+
+
+deactivate

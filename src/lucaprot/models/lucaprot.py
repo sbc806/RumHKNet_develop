21dab99e8d66a7708d9c7ac64481f0798b7d5064
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-12 06:26:13 -0800

    Update lucaprot.py

--- src/lucaprot/models/lucaprot.py
+++ src/lucaprot/models/lucaprot.py
@@ -275,7 +275,6 @@ class LucaProt(BertPreTrainedModel):
         # dropout layer
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-        self.device = args.device
         # output layer
         self.output_mode = args.output_mode
         self.num_batches = args.num_batches
@@ -581,9 +580,9 @@ class LucaProt(BertPreTrainedModel):
 
         # Create batch tensor
         if batches is not None:
-            batches_one_hot = torch.zeros(len(batches), self.num_batches, dtype=torch.int64).to(self.device)
-            selected_one_hot = nn.functional.one_hot(batches.to(self.device)[batches.to(self.device)>=0], self.num_batches)
-            batches_one_hot[batches.to(self.device)>=0] = selected_one_hot
+            batches_one_hot = torch.zeros(len(batches), self.num_batches, dtype=torch.int64).to("cuda")
+            selected_one_hot = nn.functional.one_hot(batches.to("cuda")[batches.to("cuda")>=0], self.num_batches)
+            batches_one_hot[batches.to("cuda")>=0] = selected_one_hot
 
             pooled_output = torch.concat((pooled_output, batches_one_hot), axis=-1)
         

4617d99b5e3874653da32ab6fc15f674fece7943
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-12 15:04:17 -0800

    Update esm_step_3_family_filtered_subword_vocab_no_matrix_dirpath.sh

--- src/training/V3/esm_step_3_family_filtered_subword_vocab_no_matrix_dirpath.sh
+++ src/training/V3/esm_step_3_family_filtered_subword_vocab_no_matrix_dirpath.sh
@@ -99,7 +99,7 @@ python -u run.py \
   --seq_subword \
   --codes_file ../subword/$tokenizer_dir/$codes_file \
   --label_filepath ../kinases_dataset/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/label.txt  \
-  --output_dir ../models/step_3/esm_family_filtered/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
+  --output_dir ../models_esm_step_3_family_filtered_subword_vocab_no_matrix_dirpath/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --log_dir ../logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --tb_log_dir ../tb-logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --config_path ../config/$MODEL_TYPE/$CONFIG_NAME \

06abb4c2f349e553ae0aba25152516fdd2429398
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-12 05:45:45 -0800

    Update run_transformer_step_3_batch.sh

--- bash_scripts_nibi/step_3/transformer/run_transformer_step_3_batch.sh
+++ bash_scripts_nibi/step_3/transformer/run_transformer_step_3_batch.sh
@@ -18,8 +18,9 @@ source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat transformer_step_3_8_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/transformer/output/transformer_step_3_batch_$SLURM_JOB_ID.txt
+cat transformer_step_3_batch.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/transformer/output/transformer_step_3_batch_$SLURM_JOB_ID.txt
 ./transformer_step_3_batch.sh
 
 
 deactivate
+

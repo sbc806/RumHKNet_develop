078512a7b8473947e298fb34d7febe26a052a487
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-13 02:09:02 -0800

    Update run_transformer_step_2_4_1.sh

--- bash_scripts_nibi/step_2/transformer/run_transformer_step_2_4_1.sh
+++ bash_scripts_nibi/step_2/transformer/run_transformer_step_2_4_1.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=4-0
+#SBATCH --time=3-0
 #SBATCH --job-name=transformer-step-2-4-1
 #SBATCH --output=output/transformer_step_2_4_1_%j.out
 #SBATCH --er=output/transformer_step_2_4_1_%j.err
@@ -17,7 +17,7 @@ source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_setp_2_4_1_$SLURM_JOB_ID.sh
+cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_setp_2_4_1_$SLURM_JOB_ID.txt
 ./transformer_step_2_4_1.sh
 
 

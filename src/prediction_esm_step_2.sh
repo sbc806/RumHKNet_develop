6f0b10383fff37e41f686b5d3fef145fcba2f45c
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-13 05:36:51 -0800

    Update prediction_esm_step_2.sh

--- src/prediction_esm_step_2.sh
+++ src/prediction_esm_step_2.sh
@@ -8,7 +8,7 @@ python prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/test_data/step_2/extra_p_2_class_v3_kinases_only/train.fasta \
     --model_path $dir_path/models_trained/step_2/esm \
-    --save_path $dir_path/predicted_results/test_data/step_2/esm/extra_p_2_class_v3_kinases_only/train_predicted.csv \
+    --save_path $dir_path/predicted_results/test_data/step_2/esm/extra_p_2_class_v3_kinases_only/train_predicted_$2.csv \
     --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \
     --task_type binary_class \
@@ -31,7 +31,7 @@ python prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/test_data/step_2/extra_p_2_class_v3_kinases_only/dev.fasta \
     --model_path $dir_path/models_trained/step_2/esm \
-    --save_path $dir_path/predicted_results/test_data/step_2/esm/extra_p_2_class_v3_kinases_only/dev_predicted.csv \
+    --save_path $dir_path/predicted_results/test_data/step_2/esm/extra_p_2_class_v3_kinases_only/dev_predicted_$2.csv \
     --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \
     --task_type binary_class \
@@ -51,7 +51,7 @@ python prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/test_data/step_2/extra_p_2_class_v3_kinases_only/test.fasta \
     --model_path $dir_path/models_trained/step_2/esm \
-    --save_path $dir_path/predicted_results/test_data/step_2/esm/extra_p_2_class_v3_kinases_only/test_predicted.csv \
+    --save_path $dir_path/predicted_results/test_data/step_2/esm/extra_p_2_class_v3_kinases_only/test_predicted_$2.csv \
     --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \
     --task_type binary_class \

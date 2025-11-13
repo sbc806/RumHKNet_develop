590643cfc0a931ef96186f962ce7043290bd41cc
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-13 06:21:04 -0800

    Update run_transformer_step_1_non_kinases_preprocessed.sh

--- bash_scripts_nibi/step_1/transformer/run_transformer_step_1_non_kinases_preprocessed.sh
+++ bash_scripts_nibi/step_1/transformer/run_transformer_step_1_non_kinases_preprocessed.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100_3g.40gb:1
 #SBATCH --mem=32G
-#SBATCH --time=4-0
+#SBATCH --time=3-0
 #SBATCH --job-name=transformer-step-1-non-kinases-preprocessed
 #SBATCH --output=output/transformer_step_1_non_kinases_preprocessed_%j.out
 #SBATCH --err=output/transformer_step_1_non_kinases_preprocessed_%j.err

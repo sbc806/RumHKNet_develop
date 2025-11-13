45d724eb5d06a4012b65d0be9ec6137ae6e465a4
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-13 03:09:15 -0800

    Update prediction_esm_step_2.sh

--- src/prediction_esm_step_2.sh
+++ src/prediction_esm_step_2.sh
@@ -1,13 +1,13 @@
 # Binary Classification
 # export CUDA_VISIBLE_DEVICES="0"
+dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
 python prediction_v2.py \
     --seq_type prot \
-    --input_file ../test_data/examples.fasta \
-    --emb_dir ../../test_data/examples/embedding/lucapcyclev3/ \
+    --input_file $dir_path/test_data/step_2/examples.fasta \
     --llm_truncation_seq_length 10240 \
     --model_path .. \
-    --save_path ../predicted_results/test_data/examples_predicted.csv \
-    --dataset_name extra_p_2_class_v3 \
+    --save_path $dir_path/predicted_results/test_data/step_2/examples_predicted.csv \
+    --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \
     --task_type binary_class \
     --task_level_type seq_level \

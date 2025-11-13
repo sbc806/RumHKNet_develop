f5fc6613a980100ba788deb5e7f4e6b1c4701a76
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-13 06:22:02 -0800

    Update run_prediction_esm_step_2_05.sh

--- bash_scripts_nibi/prediction/step_2/esm/run_prediction_esm_step_2_05.sh
+++ bash_scripts_nibi/prediction/step_2/esm/run_prediction_esm_step_2_05.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=32G
-#SBATCH --time=3-0
+#SBATCH --time=7-0
 #SBATCH --job-name=prediction-esm-step-2
 #SBATCH --output=output/prediction_esm_step_2_%j.out
 #SBATCH --er=output/prediction_esm_step_2_%j.err

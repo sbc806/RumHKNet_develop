88b9570dd29c9d8709297e61e1e9c3a11b3b1efb
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 09:18:20 -0800

    Update run_esm_step_3_batch_4_1_no_matrix_dirpath.sh

---
+++ bash_scripts_nibi/step_3/esm/run_esm_step_3_batch_4_1_no_matrix_dirpath.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --mem=40G
+#SBATCH --time=3-0
+#SBATCH --job-name=esm-step-3-batch-4-1-no-matrix-dirpath
+#SBATCH --output=output/esm_step_3_batch_4_1_no_matrix_dirpath_%j.out
+#SBATCH --err=output/esm_step_3_batch_4_1_no_matrix_dirpath_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat esm_step_3_batch_4_1_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/esm/output/esm_step_3_batch_4_1_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./esm_step_3_batch_4_1_no_matrix_dirpath.sh
+
+
+deactivate

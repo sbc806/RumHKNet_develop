d151e51ce8d28128dc41c7fd549dac0d7d536b4e
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 08:48:49 -0800

    Update run_transformer_step_3_family_filtered_4_1.sh

---
+++ bash_scripts_nibi/step_3/transformer/run_transformer_step_3_family_filtered_4_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --mem=32G
+#SBATCH --time=3-0
+#SBATCH --job-name=transformer-step-3-family_filtered_4_1
+#SBATCH --output=output/transformer_step_3_family_filtered_4_1_%j.out
+#SBATCH --err=output/transformer_step_3_family_filtered_4_1_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat transformer_step_3_family_filtered_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/transformer/output/transformer_step_3_family_filtered_4_1_$SLURM_JOB_ID.txt
+./transformer_step_3_family_filtered_4_1.sh
+
+
+deactivate

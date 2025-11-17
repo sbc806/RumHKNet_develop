bdb12f96674ac27dff5fbdb06d679d1f176fd0d7
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-17 09:17:38 -0800

    Update run_both_step_3_batch_4_1_no_matrix_dirpath.sh

---
+++ bash_scripts_nibi/step_3/both/run_both_step_3_batch_4_1_no_matrix_dirpath.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus=h100:1
+#SBATCH --mem=100G
+#SBATCH --time=5-0
+#SBATCH --job-name=both-step-3-batch-4-1-no-matrix-dirpath
+#SBATCH --output=output/both_step_3_batch_4_1_no_matrix_dirpath_%j.out
+#SBATCH --err=output/both_step_3_batch_4_1_no_matrix_dirpath_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat both_step_3_batch_4_1_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/both/output/both_step_3_batch_4_1_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./both_step_3_batch_4_1_no_matrix_dirpath.sh
+
+
+deactivate

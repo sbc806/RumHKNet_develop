d9e7b50c10cae013dbb5fb42bb230da23a295696
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-20 23:59:49 -0800

    Update run_esm_step_3_batch_no_matrix_dirpath.sh

--- bash_scripts_nibi/step_3/esm/run_esm_step_3_batch_no_matrix_dirpath.sh
+++ bash_scripts_nibi/step_3/esm/run_esm_step_3_batch_no_matrix_dirpath.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --account=rrg-guanuofa
+#SBATCH --account=def-guanuofa
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
-#SBATCH --mem=40G
-#SBATCH --time=3-0
+#SBATCH --mem=80G
+#SBATCH --time=7-0
 #SBATCH --job-name=esm-step-3-batch-no-matrix-dirpath
 #SBATCH --output=output/esm_step_3_batch_no_matrix_dirpath_%j.out
 #SBATCH --err=output/esm_step_3_batch_no_matrix_dirpath_%j.err

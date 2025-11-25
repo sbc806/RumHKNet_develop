ea2afbec3888f3b34411fbd36358b095b559b833
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-24 18:22:16 -0800

    Update prediction_both_step_1.sh

---
+++ src/prediction_scripts/step_1/both/prediction_both_step_1.sh
@@ -0,0 +1,25 @@
+# Binary Classification
+export CUDA_VISIBLE_DEVICES="0"
+dir_path="/home/schen123/projects/rrg-guanuofa/schen123/kinases"
+threshold=$1
+echo $threshold
+
+python -u prediction_v2.py \
+    --seq_type prot \
+    --input_file $dir_path/predictions/predictions_datasets/step_1/both/$3.csv \
+    --model_path $dir_path/models_trained/step_2/esm \
+    --save_path $dir_path/predicted_results/step_1/both/$3_predicted_$2.csv \
+    --dataset_name extra_p_2_class_v3_kinases_only \
+    --dataset_type protein \
+    --task_type binary_class \
+    --task_level_type seq_level \
+    --model_type lucaprot \
+    --input_type matrix \
+    --input_mode single \
+    --time_str 20250913223115 \
+    --step 142912 \
+    --threshold $threshold \
+    --print_per_num 100000 \
+    --truncation_seq_length 10240 \
+    --emb_dir $dir_path/embeddings/step_1/esm \
+    --gpu_id 0

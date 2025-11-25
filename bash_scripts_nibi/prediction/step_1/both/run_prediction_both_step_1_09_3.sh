e9dc79a7c2633db2d6026938e4e14686dd2e87a4
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-25 12:56:07 -0800

    Update run_prediction_both_step_1_09_3.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_09_3.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_09_3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_2g.20gb:1
-#SBATCH --mem=25G
+#SBATCH --mem=64G
 #SBATCH --time=3-0
 #SBATCH --job-name=prediction-both-step-1-09-3
 #SBATCH --output=output/prediction_both_step_1_09_3_%j.out

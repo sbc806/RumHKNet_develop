df87a874fdfc2eb9cb2cf67f902e8a93f5a9fa96
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-25 13:30:25 -0800

    Update prediction_both_step_2.sh

--- src/prediction_scripts/step_2/both/prediction_both_step_2.sh
+++ src/prediction_scripts/step_2/both/prediction_both_step_2.sh
@@ -1,13 +1,13 @@
 # Binary Classification
 export CUDA_VISIBLE_DEVICES="0"
-dir_path="/home/schen123/links/projects/def-guanuofa/schen123/kinases"
+dir_path="/home/schen123/projects/def-guanuofa/schen123/kinases"
 threshold=$1
 echo $threshold
 
 python -u prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_2/$3.csv \
-    --model_path $dir_path/sbc806_3/RumHKNet/models/step_2/both \
+    --model_path $dir_path/sbc806/RumHKNet/models/step_2/both \
     --save_path $dir_path/predictions/predicted_results/step_2/both/$3_predicted_$2.csv \
     --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \

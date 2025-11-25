dfc2b20242b028aa4825a5b28ee12fc5f9061f90
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-24 21:46:03 -0800

    Update run_prediction_both_step_2_02_1.sh

--- bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_02_1.sh
+++ bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_02_1.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806_4/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.3 02 train_1
+./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.2 02 train_1
 
 
 deactivate

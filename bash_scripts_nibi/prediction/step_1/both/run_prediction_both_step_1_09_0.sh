e1bcd765cec50e8eba7be5c1107cb783b902b37a
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-25 12:40:02 -0800

    Update run_prediction_both_step_1_09_0.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_09_0.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_09_0.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
-#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
-#SBATCH --mem=64G
-#SBATCH --time=0:10:0
-#SBATCH --job-name=prediction-both-step-1-five-sequences
-#SBATCH --output=output/prediction_both_step_1_five_sequences_%j.out
-#SBATCH --er=output/prediction_both_step_1_five_sequences_%j.err
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_2g.20gb:1
+#SBATCH --mem=25G
+#SBATCH --time=3-0
+#SBATCH --job-name=prediction-both-step-1-09-0
+#SBATCH --output=output/prediction_both_step_1_09_0_%j.out
+#SBATCH --er=output/prediction_both_step_1_09_0_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.5 05 train_five_sequences
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.9 09 train_0
 
 
 deactivate

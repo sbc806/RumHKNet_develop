3340189c3e44922dcabc8afe4bd7f15bcc1d129b
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-25 12:44:30 -0800

    Update run_prediction_both_step_1_09_1.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_09_1.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_09_1.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=3-0
 #SBATCH --job-name=prediction-both-step-1-09-1
 #SBATCH --output=output/prediction_both_step_1_09_1_%j.out
-#SBATCH --er=output/prediction_both_step_1_09_1_%j.err
+#SBATCH --err=output/prediction_both_step_1_09_1_%j.err
 
 
 module load python/3.11

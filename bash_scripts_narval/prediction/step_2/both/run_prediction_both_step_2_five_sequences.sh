da12c39a104eaef61d5227280eb559589d1c50f9
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-26 00:03:37 -0800

    Update run_prediction_both_step_2_five_sequences.sh

---
+++ bash_scripts_narval/prediction/step_2/both/run_prediction_both_step_2_five_sequences.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=a100_4g.20gb:1
+#SBATCH --mem=64G
+#SBATCH --time=0:10:0
+#SBATCH --job-name=prediction-both-step-2-five-sequences
+#SBATCH --output=output/prediction_both_step_2_five_sequences_%j.out
+#SBATCH --er=output/prediction_both_step_2_five_sequences_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.5 05 train_five_sequences
+
+
+deactivate
+

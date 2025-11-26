6835f4805442e6cb8bc2a54a2628433d80a6f902
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-26 02:42:23 -0800

    Update run_prediction_both_step_1_xaa.sh

--- bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_xaa.sh
+++ bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_xaa.sh
@@ -3,16 +3,16 @@
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_2g.20gb:1
 #SBATCH --mem=64G
 #SBATCH --time=7-0
-#SBATCH --job-name=prediction-both-step-1-xaa-new-sequences
-#SBATCH --output=output/prediction_both_step_1_xaa_new_02_%j.out
-#SBATCH --err=output/prediction_both_step_1_xaa_new_02_%j.err
+#SBATCH --job-name=prediction-both-step-1-five-sequences
+#SBATCH --output=output/prediction_both_step_1_five_sequences_%j.out
+#SBATCH --er=output/prediction_both_step_1_five_sequences_%j.err
 
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/21.0.0
+module load gcc arrow/19.0.1
 
-cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+cd /home/schen123/scratch/kinases/virtual_environments
 source TEST/bin/activate
 
 
@@ -22,6 +22,8 @@ cd ../sbc806/RumHKNet/src/
 
 
 deactivate
+
+
 
 
 

5d77a801875cdbd13a4452a21dcf91416164afd3
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-28 20:17:12 -0800

    Update prediction_both_step_1.sh

--- src/prediction_scripts/step_1/both/prediction_both_step_1.sh
+++ src/prediction_scripts/step_1/both/prediction_both_step_1.sh
@@ -20,7 +20,7 @@ python -u prediction_v2.py \
     --step 374392 \
     --threshold $threshold \
     --print_per_num 100000 \
-    --truncation_seq_length 10240 \
+    --truncation_seq_length 3432 \
     --emb_dir $dir_path/embeddings/step_1/esm \
     --gpu_id 0 \
     --seq_id_idx 0 \

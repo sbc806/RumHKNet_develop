494964239a824cfaf069d60e6c71ce0cc7a81038
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-11-28 20:23:20 -0800

    Update run_prediction_both_step_1_five_sequences.sh

--- bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_five_sequences.sh
+++ bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_five_sequences.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-guanuofa
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.20gb:1
 #SBATCH --mem=32G
-#SBATCH --time=3-0
+#SBATCH --time=0:5:0
 #SBATCH --job-name=prediction-both-step-1-five-sequences
 #SBATCH --output=output/prediction_both_step_1_five_sequences_%j.out
 #SBATCH --er=output/prediction_both_step_1_five_sequences_%j.err

ae622911d5f96c6c59430611fa4058a8a4c93aae
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 06:29:07 -0800

    Update run_prediction_both_step_1_five_sequences.sh

--- bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_five_sequences.sh
+++ bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_five_sequences.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.5 05 train_five_sequences 130 _test
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.5 05 train_five_sequences 130 _test_1
 
 
 deactivate

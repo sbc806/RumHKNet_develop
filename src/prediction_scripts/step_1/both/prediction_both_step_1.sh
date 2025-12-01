d209932437b0c4bc4777b56de8087e778ba0b3b7
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 03:04:42 -0800

    Update prediction_both_step_1.sh

--- src/prediction_scripts/step_1/both/prediction_both_step_1.sh
+++ src/prediction_scripts/step_1/both/prediction_both_step_1.sh
@@ -5,12 +5,13 @@ threshold=$1
 echo $threshold
 truncation_seq_length=$3
 echo $truncation_seq_length
-
+extra=$5
+echo $extra
 python -u prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_1/$3.csv \
     --model_path $dir_path/sbc806/RumHKNet/models_step_1_both \
-    --save_path $dir_path"/predictions/predicted_results/step_1/both/$3_predicted_$2"_$truncation_seq_length$4.csv \
+    --save_path $dir_path"/predictions/predicted_results/step_1/both/$3_predicted_$2"_$truncation_seq_length$extra.csv \
     --dataset_name step_1_non_kinases_preprocessed \
     --dataset_type protein \
     --task_type binary_class \

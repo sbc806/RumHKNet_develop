f7a37c9c15ec6a024ed85752bb9273eb361724d5
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 02:54:25 -0800

    Update encoder.py

--- src/encoder.py
+++ src/encoder.py
@@ -487,7 +487,7 @@ class Encoder(object):
                 raise Exception("matrix is not filepath-str and np.ndarray")
 
         seq = seq.strip().upper()
-        print(matrix.shape)
+        print("matrix:",matrix.shape)
         return {
             "seq_id": seq_id,
             "seq": seq,

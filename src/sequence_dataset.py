14ecbb10f35fc69f460fcb5037ae3d49ae7f71a7
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 08:37:14 -0800

    Update sequence_dataset.py

--- src/sequence_dataset.py
+++ src/sequence_dataset.py
@@ -7,7 +7,7 @@ class SequenceDataset(Dataset)
   def __init__(self, dir_path, sequences_file):
       self.sequences_df = pd.read_csv(os.path.join(dir_path, sequences_file))
 
-  def __len(self):
+  def __len__(self):
       return len(self.sequences_df)
 
   def __getitem__(self, id):
@@ -15,5 +15,6 @@ class SequenceDataset(Dataset)
       seq_id = self.sequences_df["seq_id"].iloc[idx]
       seq_type = self.sequences_df["type"].iloc[idx]
       return {"seq": seq, "seq_id": seq_id, "seq_type": seq_type}
+
 
 

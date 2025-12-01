ca2925f4716aa2605213e84f17fd9de0e83e72a1
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 09:06:46 -0800

    Update sequence_dataset.py

--- src/sequence_dataset.py
+++ src/sequence_dataset.py
@@ -14,7 +14,12 @@ class SequenceDataset(Dataset)
       seq = self.sequences_df["seq"].iloc[idx]
       seq_id = self.sequences_df["seq_id"].iloc[idx]
       seq_type = self.sequences_df["type"].iloc[idx]
-      return {"seq": seq, "seq_id": seq_id, "seq_type": seq_type}
+      if self.trunc_type == "left":
+        seq_truncated = seq[-truncation_seq_length:]
+      else:
+        seq_truncated = seq[:truncation_seq_length]
+      return {"transformer": [seq_id, seq], "esm": [seq_id, seq_truncated]}
+
 
 
 

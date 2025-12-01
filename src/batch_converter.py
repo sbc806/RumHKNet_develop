bb407dcb80483bdf3cd77254582d14898bd3a5cb
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 06:24:25 -0800

    Update batch_converter.py

--- src/batch_converter.py
+++ src/batch_converter.py
@@ -535,7 +535,7 @@ class BatchConverter(object):
                 labels = torch.tensor(new_labels, dtype=torch.int64)
         else:
             labels = None
-
+        print("encoded_matrices:",encoded_matrices.shape)
         return input_ids, \
                position_ids, \
                token_type_ids, \

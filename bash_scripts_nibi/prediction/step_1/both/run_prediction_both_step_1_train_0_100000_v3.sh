1566ad943c5e3654de154481b36db692df0ed390
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-02 07:22:06 -0800

    Update run_prediction_both_step_1_train_0_100000_v3.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_train_0_100000_v3.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_train_0_100000_v3.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.3 03 train_0_100000_new 3432 100 _v3
+./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.3 03 train_0_100000_new 3432 10 _v3
 
 
 deactivate

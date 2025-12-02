646d17d9dbc5f67b32e11247b5980b07d2a9dbba
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-01 22:05:21 -0800

    Update predict_embedding.py

--- src/esm/predict_embedding.py
+++ src/esm/predict_embedding.py
@@ -525,7 +525,7 @@ def predict_embedding_multiple(samples,
         elif version == "3B":
             llm_name = "esm2_t36_3B_UR50D"
             global_layer_size = 36
-            # global_model, global_alphabet = pretrained.load_model_and_alphabet("esm2_t36_3B_UR50D")
+            global_model, global_alphabet = pretrained.load_model_and_alphabet("esm2_t36_3B_UR50D")
 
             # model_name = "esm2_t36_3B_UR50D"
             # url = f"https://dl/fbaipublicfiles.com/fair-esm/regression/{model_name}.pt"
@@ -536,17 +536,17 @@ def predict_embedding_multiple(samples,
                 # weights_only=False
             # )
 
-            model_name = "esm2_t36_3B_UR50D"
-            url = f"https://dl.fbaipublicfiles.com/fair-esm/regression/{model_name}-contact-regression.pt"
-            fn = Path(url).name
-            regression_data = torch.load(
-            f"{torch.hub.get_dir()}/checkpoints/{fn}",
-            map_location="cpu",
-            weights_only=False
-        )
+            # model_name = "esm2_t36_3B_UR50D"
+            # url = f"https://dl.fbaipublicfiles.com/fair-esm/regression/{model_name}-contact-regression.pt"
+            # fn = Path(url).name
+            # regression_data = torch.load(
+            # f"{torch.hub.get_dir()}/checkpoints/{fn}",
+            # map_location="cpu",
+            # weights_only=False
+        # )
 
-            model_name = "esm2_t36_3B_UR50D"
-            global_model, global_alphabet = pretrained.load_model_and_alphabet_core(model_name, model_data, regression_data)
+            # model_name = "esm2_t36_3B_UR50D"
+            # global_model, global_alphabet = pretrained.load_model_and_alphabet_core(model_name, model_data, regression_data)
         elif version == "650M":
             llm_name = "esm2_t33_650M_UR50D"
             global_layer_size = 33

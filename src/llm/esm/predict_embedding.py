7e1629c6ccd4242c33605caab2ad434a732f65f1
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-02 03:34:07 -0800

    Update predict_embedding.py

--- src/llm/esm/predict_embedding.py
+++ src/llm/esm/predict_embedding.py
@@ -487,7 +487,7 @@ def predict_embedding(sample,
                 print("Please reduce the 'truncation_seq_length'")
             return None, None
 
-def predict_embedding_multiple(sample,
+def predict_embedding_multiple(seq_batch,
                       trunc_type,
                       embedding_type,
                       repr_layers=[-1],
@@ -510,16 +510,16 @@ def predict_embedding_multiple(sample,
     global global_model, global_alphabet, global_version, global_layer_size
     assert "bos" in embedding_type or "representations" in embedding_type \
            or "matrix" in embedding_type or "vector" in embedding_type or "contacts" in embedding_type
-    if len(sample) > 2:
-        protein_id, protein_seq = sample[0], sample[2]
+    # if len(sample) > 2:
+        # protein_id, protein_seq = sample[0], sample[2]
+    # else:
+        # protein_id, protein_seq = sample[0], sample[1]
+    # protein_seq = clean_seq(protein_id, protein_seq)
+    # if len(protein_seq) > truncation_seq_length:
+    if trunc_type == "left":
+        seq_batch.loc[:, "seq"] = seq_batch["seq"].str[-truncation_seq_length:]
     else:
-        protein_id, protein_seq = sample[0], sample[1]
-    protein_seq = clean_seq(protein_id, protein_seq)
-    if len(protein_seq) > truncation_seq_length:
-        if trunc_type == "left":
-            protein_seq = protein_seq[-truncation_seq_length:]
-        else:
-            protein_seq = protein_seq[:truncation_seq_length]
+        seq_batch.loc[:, "seq"] = seq_batch["seq"][:truncation_seq_length]
     if global_model is None or global_alphabet is None or global_version is None or global_version != version or global_layer_size is None:
         if version == "15B":
             llm_name = "esm2_t48_15B_UR50D"
@@ -588,7 +588,7 @@ def predict_embedding_multiple(sample,
     global_model.eval()
 
     converter = BatchConverter(global_alphabet, truncation_seq_length)
-    protein_ids, raw_seqs, tokens = converter([[protein_id, protein_seq]])
+    protein_ids, raw_seqs, tokens = converter(seq_batch.values.tolist())
     embeddings = {}
     with torch.no_grad():
         # if torch.cuda.is_available():

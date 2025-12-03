44c41ce7acf4ab57a2db9267dec216a535989687
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-03 04:42:58 -0800

    Update run_prediction_both_step_1_34551_v2.sh

---
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_34551_v2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=64G
+#SBATCH --time=1-0
+#SBATCH --job-name=prediction-both-step-1-sequences-34551-v2
+#SBATCH --output=output/prediction_both_step_1_sequences_34551_v2_%j.out
+#SBATCH --err=output/prediction_both_step_1_sequences_34551_v2_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 sequences_34551 34551 v2
+
+
+deactivate
+

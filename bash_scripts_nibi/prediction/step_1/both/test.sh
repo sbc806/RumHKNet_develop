da9e19363560bf0f8bec755f8c4a113c5ba89afe
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-03 20:27:05 -0800

    Update test.sh

---
+++ bash_scripts_nibi/prediction/step_1/both/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+cat ./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh
+
+
+deactivate
+
+
+
+

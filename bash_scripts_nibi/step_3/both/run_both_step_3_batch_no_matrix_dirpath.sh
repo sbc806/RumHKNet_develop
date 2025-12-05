d43cf0c807d7b072db399198281f7159afa28440
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-05 02:42:13 -0800

    Update run_both_step_3_batch_no_matrix_dirpath.sh

--- bash_scripts_nibi/step_3/both/run_both_step_3_batch_no_matrix_dirpath.sh
+++ bash_scripts_nibi/step_3/both/run_both_step_3_batch_no_matrix_dirpath.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-guanuofa
-#SBATCH --gpus=nvidia_h100:1
+#SBATCH --gpus=nvidia_h100_80gbm:1
 #SBATCH --mem=100G
 #SBATCH --time=7-0
 #SBATCH --job-name=both-step-3-batch-no-matrix-dirpath

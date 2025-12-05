2bc1993d242f44cbb4315309344c5b59c2e66c80
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-04 23:59:15 -0800

    Update prediction_both_step_1_v3.sh

--- src/prediction_scripts/step_1/both/prediction_both_step_1_v3.sh
+++ src/prediction_scripts/step_1/both/prediction_both_step_1_v3.sh
@@ -12,7 +12,7 @@ echo $extra
 
 python -u prediction_v3.py \
     --seq_type prot \
-    --input_file $dir_path/predictions/predictions_dataset/step_1/$3.csv \
+    --input_file $dir_path/predictions/predictions_dataset/step_1/clustered/$3.csv \
     --model_path $dir_path/sbc806/RumHKNet/models_step_1_both \
     --save_path $dir_path"/predictions/predicted_results/step_1/both/clustered \
     --dataset_name step_1_non_kinases_preprocessed \

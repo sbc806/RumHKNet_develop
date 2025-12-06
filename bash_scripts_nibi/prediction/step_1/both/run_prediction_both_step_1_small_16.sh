b4c568bfab9efa27678f458e70e80124a4722d76
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-05 19:45:39 -0800

    Update run_prediction_both_step_1_small_16.sh

---
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_small_16.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=7-0
+#SBATCH --job-name=prediction-both-step-1-small-16-v3
+#SBATCH --output=output/prediction_both_step_1_small_16_v3_%j.out
+#SBATCH --err=output/prediction_both_step_1_small_16_v3_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.3 03 clustered_rep_seq95_small_16 1500 50 _v3
+
+
+deactivate

5ab8c03f8e538f06ff040016011c93ce7b31c778
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-09 19:41:27 -0800

    Update run_both_step_3_11_family_512_no_matrix_dirpath.sh

---
+++ bash_scripts_fir/step_3/both/run_both_step_3_11_family_512_no_matrix_dirpath.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --mem=100G
+#SBATCH --time=3-0
+#SBATCH --job-name=both-step-3-11-512-family-no-matrix-dirpath
+#SBATCH --output=output/both_step_3_11_512_family_no_matrix_dirpath_%j.out
+#SBATCH --err=output/both_step_3_11_512_family_no_matrix_dirpath_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+
+cd /home/schen123/scratch/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat both_step_3_11_family_512_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/both/output/both_step_3_11_family_512_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./both_step_3_11_family_512_no_matrix_dirpath.sh
+
+
+deactivate
+
+
+

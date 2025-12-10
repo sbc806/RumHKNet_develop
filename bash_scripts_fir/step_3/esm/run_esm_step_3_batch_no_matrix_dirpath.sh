ed63b6140f85c074c6245aa09ea092f5fce414c3
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-09 18:53:05 -0800

    Update run_esm_step_3_batch_no_matrix_dirpath.sh

--- bash_scripts_fir/step_3/esm/run_esm_step_3_batch_no_matrix_dirpath.sh
+++ bash_scripts_fir/step_3/esm/run_esm_step_3_batch_no_matrix_dirpath.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=100G
 #SBATCH --time=7-0
-#SBATCH --job-name=both-step-3-batch-no-matrix-dirpath
-#SBATCH --output=output/both_step_3_batch_no_matrix_dirpath_%j.out
-#SBATCH --err=output/both_step_3_batch_no_matrix_dirpath_%j.err
+#SBATCH --job-name=esm-step-3-batch-no-matrix-dirpath
+#SBATCH --output=output/esm_step_3_batch_no_matrix_dirpath_%j.out
+#SBATCH --err=output/esm_step_3_batch_no_matrix_dirpath_%j.err
 
 
 module load python/3.11
@@ -18,10 +18,11 @@ source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat both_step_3_batch_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/both/output/both_step_3_batch_no_matrix_dirpath_$SLURM_JOB_ID.txt
-./both_step_3_batch_no_matrix_dirpath.sh
+cat esm_step_3_batch_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/both/output/both_step_3_batch_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./esm_step_3_batch_no_matrix_dirpath.sh
 
 
 deactivate
+
 
 

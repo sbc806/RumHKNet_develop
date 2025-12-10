4773594272482269054f633e0724e0869ebf704d
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-09 22:02:22 -0800

    Update run_prediction_both_step_1_small_10_remaining.sh

---
+++ bash_scripts_rorqual/prediction/step_1/both/run_prediction_both_step_1_small_10_remaining.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=3-0
+#SBATCH --job-name=prediction-both-step-1-small-10-remaining-v3
+#SBATCH --output=output/prediction_both_step_1_small_10_remaining_v3_%j.out
+#SBATCH --err=output/prediction_both_step_1_small_10_remaining_v3_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806_4/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1/both/prediction_both_step_1_v3_rorqual.sh 0.3 03 clustered_rep_seq95_small_10_remaining 1500 50 _v3
+
+
+deactivate
+
+

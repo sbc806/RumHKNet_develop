7d11d02bf050d075d4036f4b8be3bcf3ca3282b0
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-09 18:54:24 -0800

    Update run_esm_step_3_batch_no_matrix_dirpath.sh

--- bash_scripts_fir/step_3/esm/run_esm_step_3_batch_no_matrix_dirpath.sh
+++ bash_scripts_fir/step_3/esm/run_esm_step_3_batch_no_matrix_dirpath.sh
@@ -18,11 +18,12 @@ source TEST/bin/activate
 
 
 cd ../sbc806/RumHKNet/src/training/V3
-cat esm_step_3_batch_no_matrix_dirpath.sh > /home/schen123/scratch/RumHKNet/bash_scripts_fir/step_3/esm/output/both_step_3_batch_no_matrix_dirpath_$SLURM_JOB_ID.txt
+cat esm_step_3_batch_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/sbc806/RumHKNet/bash_scripts_fir/step_3/esm/output/both_step_3_batch_no_matrix_dirpath_$SLURM_JOB_ID.txt
 ./esm_step_3_batch_no_matrix_dirpath.sh
 
 
 deactivate
+
 
 
 

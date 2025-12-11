adc254b815794dba9926345acd3c971f7efc095a
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-11 08:09:19 -0800

    Update run_prediction_both_step_1_large_last_1250.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_large_last_1250.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_large_last_1250.sh
@@ -18,8 +18,9 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 sequences_34551_v2 34551 _v2
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 clustered_rep_seq95_large_last_1250 34551 _v2
 
 
 deactivate
+
 

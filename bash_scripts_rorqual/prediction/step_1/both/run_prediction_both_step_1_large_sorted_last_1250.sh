25a135db6be499e183638054b9530c96460ca21a
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-11 08:40:16 -0800

    Update run_prediction_both_step_1_large_sorted_last_1250.sh

---
+++ bash_scripts_rorqual/prediction/step_1/both/run_prediction_both_step_1_large_sorted_last_1250.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=3-0
+#SBATCH --job-name=prediction-both-step-1-sequences-large-sorted-last-1250
+#SBATCH --output=output/prediction_both_step_1_sequences_large_sorted_last_1250_%j.out
+#SBATCH --err=output/prediction_both_step_1_sequences_large_sorted_last_1250_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806_4/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 clustered_rep_seq95_large_sorted_last_1250 34551 _v2
+
+
+deactivate
+
+
+
+

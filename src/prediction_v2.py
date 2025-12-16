589d9780a5165f146ba243073d3de3823f1cb30d
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-16 00:15:32 -0800

    Update prediction_v2.py

--- src/prediction_v2.py
+++ src/prediction_v2.py
@@ -503,7 +503,7 @@ def create_encoder_batch_convecter(
         "input_type": lucapcycle_args.input_type,
         "trunc_type": lucapcycle_args.trunc_type,
         # "seq_max_length": lucapcycle_args.truncation_matrix_length,
-        "seq_max_length": 14002,
+        "seq_max_length": 13952,
         "prepend_bos": True,
         "append_eos": True,
         "vector_dirpath": lucapcycle_args.vector_dirpath,

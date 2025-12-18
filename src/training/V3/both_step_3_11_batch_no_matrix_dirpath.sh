baaa848a8756c3a8ac16992887af447a84e9915c
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-17 19:01:27 -0800

    Update both_step_3_11_batch_no_matrix_dirpath.sh

--- src/training/V3/both_step_3_11_batch_no_matrix_dirpath.sh
+++ src/training/V3/both_step_3_11_batch_no_matrix_dirpath.sh
@@ -100,7 +100,7 @@ python -u run.py \
   --seq_subword \
   --codes_file ../subword/$tokenizer_dir/$codes_file \
   --label_filepath ../kinases_dataset/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/label.txt  \
-  --output_dir ../models_both_step_3_batch_1/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
+  --output_dir ../models_both_step_3_11_batch/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --log_dir ../logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --tb_log_dir ../tb-logs/$DATASET_NAME/$DATASET_TYPE/$TASK_TYPE/$MODEL_TYPE/$INPUT_TYPE/$time_str \
   --config_path ../config/$MODEL_TYPE/$CONFIG_NAME \

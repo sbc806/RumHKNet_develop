7e79a786527a67dd335e25f0e6a6ab79a4871f8d
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-18 19:25:46 -0800

    Update prediction_both_step_2.sh

--- src/prediction_scripts/step_2/both/prediction_both_step_2.sh
+++ src/prediction_scripts/step_2/both/prediction_both_step_2.sh
@@ -24,7 +24,8 @@ python -u prediction_v2.py \
     --emb_dir $dir_path/embeddings/step_2/esm \
     --gpu_id 0 \
     --seq_id_idx 0 \
-    --seq_idx 1
+    --seq_idx 1 \
+    --matrix_embedding_exists
 
 
 

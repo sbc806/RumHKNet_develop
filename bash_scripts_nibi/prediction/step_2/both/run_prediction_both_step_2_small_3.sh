1b5fe862e774165430079542878e62ba466e252d
sbc806 <76720208+sbc806@users.noreply.github.com>
2025-12-18 20:02:52 -0800

    Update run_prediction_both_step_2_small_3.sh

--- bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_small_3.sh
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_small_3.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=7-0
+#SBATCH --time=3-0
 #SBATCH --job-name=prediction-both-step-2-small-3-v3
 #SBATCH --output=output/prediction_both_step_2_small_3_v3_%j.out
 #SBATCH --err=output/prediction_both_step_2_small_3_v3_%j.err
@@ -22,5 +22,6 @@ cd ../sbc806/RumHKNet/src/
 
 
 deactivate
+
 
 

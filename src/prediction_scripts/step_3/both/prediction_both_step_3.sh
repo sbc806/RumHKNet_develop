466691aaec4f5d2140de9c8975d23cbb95c3454e
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-01 14:21:07 -0800

    Update prediction_both_step_3.sh

--- src/prediction_scripts/step_3/both/prediction_both_step_3.sh
+++ src/prediction_scripts/step_3/both/prediction_both_step_3.sh
@@ -7,11 +7,11 @@ echo $threshold
 python -u prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_3/clustered/$3.csv \
-    --model_path $dir_path/sbc806/RumHKNet/models_both_step_3_11_family \
+    --model_path $dir_path/sbc806/RumHKNet/models_step_3_both_11_family \
     --save_path $dir_path/predictions/predicted_results/step_3/both/$3_predicted_$2.csv \
     --dataset_name step_3_11_family \
     --dataset_type protein \
-    --task_type binary_class \
+    --task_type multi_class \
     --task_level_type seq_level \
     --model_type lucaprot \
     --input_type seq_matrix \
@@ -22,11 +22,12 @@ python -u prediction_v2.py \
     --print_per_num 100000 \
     --truncation_seq_length 12211 \
     --truncation_matrix_length 12211 \
-    --emb_dir $dir_path/embeddings/step_2/esm \
+    --emb_dir $dir_path/embeddings/step_3/esm \
     --gpu_id 0 \
     --seq_id_idx 0 \
     --seq_idx 1 \
     --matrix_embedding_exists
+
 
 
 

622046138df222f1016e12b00e6e8850e9f037fc
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-01 15:55:09 -0800

    Update prediction_both_step_3_v3.sh

--- src/prediction_scripts/step_3/both/prediction_both_step_3_v3.sh
+++ src/prediction_scripts/step_3/both/prediction_both_step_3_v3.sh
@@ -22,7 +22,7 @@ python -u prediction_v3.py \
     --model_type lucaprot \
     --input_type seq_matrix \
     --input_mode single \
-    --time_str 202511120222923 \
+    --time_str 20251120222923 \
     --step 133628 \
     --threshold $threshold \
     --print_per_num 100000 \
@@ -35,6 +35,7 @@ python -u prediction_v3.py \
     --matrix_embedding_exists \
     --chunk_size $chunk_size \
     --save_name $3_predicted_$2_$4$extra
+
 
 
 

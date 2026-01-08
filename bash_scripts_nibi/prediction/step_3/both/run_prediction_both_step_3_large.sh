9ba33a84f27e35727261109eb92cc568b929b090
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-08 01:15:21 -0800

    Update run_prediction_both_step_3_large.sh

--- bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_large.sh
+++ bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_large.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_3/both/prediction_both_step_3.sh 0.3 03 clustered_rep_seq95_large_histidine_kinase
+./prediction_scripts/step_3/both/prediction_both_step_3.sh 0.3 03 clustered_rep_seq95_large_histidine_kinase 34551
 
 
 deactivate

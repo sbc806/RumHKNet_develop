d81194cec0f657e3ada88673af63b9b1c5ac42db
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-08 11:53:49 -0800

    Update run_prediction_both_step_2_new_small_1.sh

--- bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_new_small_1.sh
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_new_small_1.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=6:0:0
+#SBATCH --time=1-0
 #SBATCH --job-name=prediction-both-step-2-small-1-v3
 #SBATCH --output=output/prediction_both_step_2_small_1_v3_%j.out
 #SBATCH --err=output/prediction_both_step_2_small_1_v3_%j.err

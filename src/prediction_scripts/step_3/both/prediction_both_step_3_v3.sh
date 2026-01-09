e6c6c77f396b121c760d956775218ad0d854ced3
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-08 17:47:57 -0800

    Update prediction_both_step_3_v3.sh

--- src/prediction_scripts/step_3/both/prediction_both_step_3_v3.sh
+++ src/prediction_scripts/step_3/both/prediction_both_step_3_v3.sh
@@ -34,7 +34,8 @@ python -u prediction_v4.py \
     --seq_idx 2 \
     --matrix_embedding_exists \
     --chunk_size $chunk_size \
-    --save_name $3_predicted_$2_$4$extra
+    --save_name $3_predicted_$2_$4$extra \
+    --topk 10
 
 
 

8ce4c52a28845aaf1193294b8a1d9ff0292e932b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-12 00:08:42 -0800

    Update run_prediction_both_step_1_new_large_0.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_new_large_0.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_new_large_0.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=2-0
-#SBATCH --job-name=prediction-both-step-1-sequences-large
-#SBATCH --output=output/prediction_both_step_1_sequences_large_%j.out
-#SBATCH --err=output/prediction_both_step_1_sequences_large_sorted_%j.err
+#SBATCH --job-name=prediction-both-step-1-sequences-large-0
+#SBATCH --output=output/prediction_both_step_1_sequences_large_0_%j.out
+#SBATCH --err=output/prediction_both_step_1_sequences_large_sorted_0_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 newrun_seqs_large 34551 _v2
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 newrun_seqs_large_0 34551 _v2
 
 
 deactivate

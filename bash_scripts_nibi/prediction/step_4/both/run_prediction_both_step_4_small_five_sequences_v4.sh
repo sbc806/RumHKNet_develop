e824db773a0b531f3cdc586f1306394f7f5be318
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-13 13:49:51 -0800

    Update run_prediction_both_step_4_small_five_sequences_v4.sh

--- bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_small_five_sequences_v4.sh
+++ bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_small_five_sequences_v4.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_4/both/prediction_both_step_4_v4.sh 0.3 03 clustered_rep_seq95_small_histidine_kinase_first_five 1500 5 _5_topk_v4
+./prediction_scripts/step_4/both/prediction_both_step_4_v4.sh 0.3 03 clustered_rep_seq95_small_histidine_kinase_batch_first_five 1500 5 _5_topk_v4
 
 
 deactivate

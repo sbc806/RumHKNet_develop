42a2820e98ff9c3a92ec3bfd2640aa8ffddb191b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-13 18:24:36 -0800

    Update lucaprot.py

--- src/lucaprot/models/lucaprot.py
+++ src/lucaprot/models/lucaprot.py
@@ -286,6 +286,7 @@ class LucaProt(BertPreTrainedModel):
             self.output = nn.Sigmoid()
         else:
             args.num_batches=10
+            self.num_batches=args.num_batches
             if args.num_batches > 1:
                 print("Using args.num_batchs:", args.num_batches)
                 self.classifier = nn.Linear(output_size+args.num_batches, config.num_labels)

6820a2f0482effe52be37e935edd92e75c529655
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-13 17:45:00 -0800

    Update prediction_both_step_4_v4.sh

--- src/prediction_scripts/step_4/both/prediction_both_step_4_v4.sh
+++ src/prediction_scripts/step_4/both/prediction_both_step_4_v4.sh
@@ -13,7 +13,7 @@ echo $extra
 python -u prediction_v4.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_4/clustered/$3.csv \
-    --model_path $dir_path/sbc806/RumHKNet/models_step_3_both_batch_1 \
+    --model_path $dir_path/sbc806/RumHKNet/models_both_step_3_batch_1 \
     --save_path $dir_path/predictions/predicted_results/step_4/both/clustered \
     --dataset_name step_3_11_family \
     --dataset_type protein \

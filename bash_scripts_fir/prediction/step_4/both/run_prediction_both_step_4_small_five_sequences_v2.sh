899044ba55b41630f1cb752a2a9b1ee4fa12e2d7
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-14 16:12:37 -0800

    Update run_prediction_both_step_4_small_five_sequences_v2.sh

--- bash_scripts_fir/prediction/step_4/both/run_prediction_both_step_4_small_five_sequences_v2.sh
+++ bash_scripts_fir/prediction/step_4/both/run_prediction_both_step_4_small_five_sequences_v2.sh
@@ -10,7 +10,7 @@
 
 module load python/3.11
 module load scipy-stack
-module load gcc arrow/21.0.0
+module load gcc arrow/19.0.1
 
 cd /home/schen123/scratch/kinases/virtual_environments
 source TEST/bin/activate
@@ -22,3 +22,4 @@ cd ../sbc806/RumHKNet/src/
 
 
 deactivate
+

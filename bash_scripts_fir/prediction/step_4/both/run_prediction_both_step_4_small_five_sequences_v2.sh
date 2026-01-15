ae803261ddfdb3395908c3019bc064ea581168e9
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-14 16:22:27 -0800

    Update run_prediction_both_step_4_small_five_sequences_v2.sh

--- bash_scripts_fir/prediction/step_4/both/run_prediction_both_step_4_small_five_sequences_v2.sh
+++ bash_scripts_fir/prediction/step_4/both/run_prediction_both_step_4_small_five_sequences_v2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=rrg-guanuofa
+#SBATCH --account=def-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=0:5:0
@@ -22,6 +22,7 @@ cd ../sbc806/RumHKNet/src/
 
 
 deactivate
+
 
 
 

1b3d0f415f2bf40d67119b52aff1c00fdf8cea1c
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-20 18:10:06 -0800

    Update run_prediction_both_step_2_ko_large.sh

--- bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_ko_large.sh
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_ko_large.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=12:0:0
-#SBATCH --job-name=prediction-both-step-2-sequences-large
-#SBATCH --output=output/prediction_both_step_2_sequences_large_%j.out
-#SBATCH --err=output/prediction_both_step_2_sequences_large_%j.err
+#SBATCH --job-name=prediction-both-step-2-sequences-ko-large
+#SBATCH --output=output/prediction_both_step_2_sequences_ko_large_%j.out
+#SBATCH --err=output/prediction_both_step_2_sequences_ko_large_%j.err
 
 
 module load python/3.11
@@ -22,6 +22,7 @@ cd ../sbc806/RumHKNet/src/
 
 
 deactivate
+
 
 
 

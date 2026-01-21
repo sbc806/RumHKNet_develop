e0bd9a5b77b25c42bb017aee49842f7cc62ba150
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-20 18:18:34 -0800

    Update run_prediction_both_step_2_blastp3050_large.sh

---
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_blastp3050_large.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=12:0:0
+#SBATCH --job-name=prediction-both-step-2-sequences-blastp3050-large
+#SBATCH --output=output/prediction_both_step_2_sequences_blastp3050_large_%j.out
+#SBATCH --err=output/prediction_both_step_2_sequences_blastp3050_large_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.3 03 2025_01_20_new_ko_shared_large
+
+
+deactivate
+
+
+
+
+
+
+
+
+
+
+
+
+

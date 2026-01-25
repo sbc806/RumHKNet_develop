be8fe1e8d33c6008a433b38f1c263b44ff52e52e
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-24 17:08:58 -0800

    Update run_prediction_both_step_1_dev.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_dev.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_dev.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=2-0
+#SBATCH --time=1-0
 #SBATCH --job-name=prediction-both-step-1-sequences-large
 #SBATCH --output=output/prediction_both_step_1_sequences_large_%j.out
 #SBATCH --err=output/prediction_both_step_1_sequences_large_sorted_%j.err
@@ -22,6 +22,7 @@ cd ../sbc806/RumHKNet/src/
 
 
 deactivate
+
 
 
 

03e6a694e4cf19c043b1ee376867e5d1633125d3
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-24 17:09:54 -0800

    Update run_prediction_both_step_1_dev.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_dev.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_dev.sh
@@ -18,10 +18,11 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 newrun_seqs_large 34551 _v2
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.3 03 step_1_non_kinases_preprocessed_dev 1500 _v2
 
 
 deactivate
+
 
 
 

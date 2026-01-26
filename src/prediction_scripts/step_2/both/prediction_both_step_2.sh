e2e79f005805fd6cb6acd48c7612244bb9f2da03
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-26 15:40:05 -0800

    Update prediction_both_step_2.sh

--- src/prediction_scripts/step_2/both/prediction_both_step_2.sh
+++ src/prediction_scripts/step_2/both/prediction_both_step_2.sh
@@ -12,7 +12,7 @@ python -u prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_2/clustered/$3.csv \
     --model_path $dir_path/sbc806/RumHKNet/models/step_2/both \
-    --save_path $dir_path/predictions/predicted_results/step_2/both/clustered/$3_predicted_$2$5_other.csv \
+    --save_path $dir_path/predictions/predicted_results/step_2/both/clustered/$3_predicted_$2$5.csv \
     --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \
     --task_type binary_class \

43f0012dd490d3e4603cde8b17c25180bc43a090
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-28 00:41:45 -0800

    Update run_prediction_both_step_3_rbags_small.sh

--- bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_rbags_small.sh
+++ bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_rbags_small.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=3-0
+#SBATCH --time=1-0
 #SBATCH --job-name=prediction-both-step-3-small-v4
 #SBATCH --output=output/prediction_both_step_3_small_v4_%j.out
 #SBATCH --err=output/prediction_both_step_3_small_v4_%j.err
@@ -18,8 +18,9 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_3/both/prediction_both_step_3_v3.sh 0.3 03 step_3_clustered_newrun_rbags_small_remaining 1500 50 _v4
+./prediction_scripts/step_3/both/prediction_both_step_3_v3.sh 0.3 03 step_3_clustered_newrun_rbags_predicted_02_small_remaining 1500 50 _v4
 
 
 deactivate
+
 

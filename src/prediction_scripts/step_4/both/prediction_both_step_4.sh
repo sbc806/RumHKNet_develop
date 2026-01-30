be68597ea64d7b0912f03aa509a52f73a9e14662
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-29 20:10:13 -0800

    Update prediction_both_step_4.sh

--- src/prediction_scripts/step_4/both/prediction_both_step_4.sh
+++ src/prediction_scripts/step_4/both/prediction_both_step_4.sh
@@ -6,9 +6,9 @@ echo $threshold
 
 python -u prediction_v2.py \
     --seq_type prot \
-    --input_file $dir_path/predictions/predictions_dataset/step_4/clustered/$3.csv \
+    --input_file $dir_path/predictions/predictions_dataset/step_4/$3.csv \
     --model_path $dir_path/sbc806/RumHKNet/models_both_step_3_batch_1 \
-    --save_path $dir_path/predictions/predicted_results/step_4/both/clustered/$3_predicted_$2$5.csv \
+    --save_path $dir_path/predictions/predicted_results/step_4/both/$3_predicted_$2$5.csv \
     --dataset_name extra_p_133_class_v3_batch \
     --dataset_type protein \
     --task_type multi_class \

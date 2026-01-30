dd9ac7ec569c45e1810b2309cd98d2efa688f341
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-01-29 23:59:40 -0800

    Update run_prediction_both_step_4_rbags_large.sh

---
+++ bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_rbags_large.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=12:0:0
+#SBATCH --job-name=prediction-both-step-4-large-v4
+#SBATCH --output=output/prediction_both_step_4_large_v4_%j.out
+#SBATCH --err=output/prediction_both_step_4_large_v4_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_4/both/prediction_both_step_4.sh 0.3 03 step_3_clustered_rep_seq95_large_histidine_kinase_batch 34551
+
+
+deactivate

5836fcd69e08e3b5311bbb91a98133de487fff1f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-08 23:06:03 -0700

    Update run_both_step_1_step_2_combined_no_matrix_dirpath.sh

---
+++ bash_scripts_nibi/step_1/both/run_both_step_1_step_2_combined_no_matrix_dirpath.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus=h100:1
+#SBATCH --mem=300G
+#SBATCH --time=7-0
+#SBATCH --job-name=both-step-1-no-matrix-dirpath
+#SBATCH --output=output/both_step_1_no_matrix_dirpath_%j.out
+#SBATCH --err=output/both_step_1_no_matrix_dirpath_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/training/V3
+cat both_step_1_step_2_combined_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/bash_scripts_nibi/step_1/both/output/both_step_1_step_2_combined_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./both_step_1_step_2_combinedno_matrix_dirpath.sh
+
+
+deactivate

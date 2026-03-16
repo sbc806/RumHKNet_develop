d9804a6f8ff172a49cab9130798ae4b7e7d25b14
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-16 16:18:11 -0700

    Update prediction_both_step_1_step_2_combined_v3.sh

--- src/prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined_v3.sh
+++ src/prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined_v3.sh
@@ -13,9 +13,9 @@ echo $extra
 
 python -u prediction_v3.py \
     --seq_type prot \
-    --input_file $dir_path/predictions/predictions_dataset/step_1/clustered/$3.csv \
+    --input_file $dir_path/predictions/predictions_dataset/step_1_step_2_combined/clustered/$3.csv \
     --model_path $dir_path_1/sbc806/RumHKNet_develop/models_step_1_step_2_combined_both \
-    --save_path $dir_path/predictions/predicted_results/step_1/both/clustered \
+    --save_path $dir_path/predictions/predicted_results/step_1_step_2_combined/both/clustered \
     --dataset_name step_1_step_2_combined \
     --dataset_type protein \
     --task_type binary_class \

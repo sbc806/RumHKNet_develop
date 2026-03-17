9561336e9714ea146888785d76e663f1a0d9a0b4
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-16 19:37:51 -0700

    Update run_prediction_step_1_step_2_combined_both_blastp_large.sh

---
+++ bash_scripts_nibi/prediction/step_1_step_2_combined/run_prediction_step_1_step_2_combined_both_blastp_large.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=1:0:0
+#SBATCH --job-name=prediction-both-step-1-step-2-combined-blastp-large
+#SBATCH --output=output/prediction_both_step_1_step_2_combined_large_blastp_%j.out
+#SBATCH --err=output/prediction_both_step_1_step_2_combined_blastp_large_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined.sh 0.2 02 histidine_ko_no_blastp_no_rumhknet_large 13000 _v2
+
+
+deactivate
+
+
+
+

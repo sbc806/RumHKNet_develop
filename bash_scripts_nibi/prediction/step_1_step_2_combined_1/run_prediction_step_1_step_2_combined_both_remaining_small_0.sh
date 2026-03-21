8e28665a37f0027415cbaa9014e51d2c24c1c447
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-21 13:39:50 -0700

    Update run_prediction_step_1_step_2_combined_both_remaining_small_0.sh

--- bash_scripts_nibi/prediction/step_1_step_2_combined_1/run_prediction_step_1_step_2_combined_both_remaining_small_0.sh
+++ bash_scripts_nibi/prediction/step_1_step_2_combined_1/run_prediction_step_1_step_2_combined_both_remaining_small_0.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=rrg-guanuofa
-#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=1-0
-#SBATCH --job-name=prediction-both-step-1-step-2-combined-remaining_small-27-v3
-#SBATCH --output=../step_1_step_2_combined/output/prediction_both_step_1_step_2_combined_remaining_small_27_v3_%j.out
-#SBATCH --err=output/prediction_both_step_1_step_2_combined_remaining_small_27_v3_%j.err
+#SBATCH --time=7-0
+#SBATCH --job-name=prediction-both-step-1-step-2-combined-remaining_small-0-v3
+#SBATCH --output=../step_1_step_2_combined/output/prediction_both_step_1_step_2_combined_remaining_small_0_v3_%j.out
+#SBATCH --err=../step_1_step_2_combined/output/prediction_both_step_1_step_2_combined_remaining_small_0_v3_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined_v3.sh 0.2 02 unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_small_27 1500 50 _v3
+./prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined_v3.sh 0.2 02 unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_small_0 1500 50 _v3
 
 
 deactivate

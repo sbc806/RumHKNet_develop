923b16e765b5144bbbf8ac275b73c64fa330ad0b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-21 01:20:43 -0700

    Update run_prediction_step_1_step_2_combined_both_remaining_small_0.sh

--- bash_scripts_nibi/prediction/step_1_step_2_combined_1/run_prediction_step_1_step_2_combined_both_remaining_small_0.sh
+++ bash_scripts_nibi/prediction/step_1_step_2_combined_1/run_prediction_step_1_step_2_combined_both_remaining_small_0.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined_v3.sh 0.2 02 unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_small_0 1500 50 _v3
+./prediction_scripts/step_1_step_2_combined/both/prediction_both_step_1_step_2_combined_v3.sh 0.2 02 unique_clustered_rep_seq_All140086RBAGs_95_90_remaining_small_27 1500 50 _v3
 
 
 deactivate

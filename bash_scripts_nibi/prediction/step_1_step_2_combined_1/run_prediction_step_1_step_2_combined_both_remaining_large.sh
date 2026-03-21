dd1a8280beb9cc53fa8432752335b581467bfa81
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-21 01:11:42 -0700

    Update run_prediction_step_1_step_2_combined_both_remaining_large.sh

--- bash_scripts_nibi/prediction/step_1_step_2_combined_1/run_prediction_step_1_step_2_combined_both_remaining_large.sh
+++ bash_scripts_nibi/prediction/step_1_step_2_combined_1/run_prediction_step_1_step_2_combined_both_remaining_large.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=3-0
-#SBATCH --job-name=prediction-both-step-1-step-2-combined-large
-#SBATCH --output=output/prediction_both_step_1_step_2_combined_large_%j.out
-#SBATCH --err=output/prediction_both_step_1_step_2_combined_large_%j.err
+#SBATCH --job-name=prediction-both-step-1-step-2-combined-large-remaining
+#SBATCH --output=../step_1_step_2_combined/output/prediction_both_step_1_step_2_combined_large_remaining_%j.out
+#SBATCH --err=../step_1_step_2_combined/output/prediction_both_step_1_step_2_combined_large_remaining_%j.err
 
 
 module load python/3.11

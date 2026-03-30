899ea6a7a590d28279c577420cfb22586c561b9b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-30 16:55:00 -0700

    Update run_prediction_both_step_1_newadd_small_3.sh

--- bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_newadd_small_3.sh
+++ bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_newadd_small_3.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=3-0
-#SBATCH --job-name=prediction-both-step-1-sequences-small-0
-#SBATCH --output=output/prediction_both_step_1_sequences_small_0_%j.out
-#SBATCH --err=output/prediction_both_step_1_sequences_small_0_%j.err
+#SBATCH --job-name=prediction-both-step-1-sequences-small-3
+#SBATCH --output=output/prediction_both_step_1_sequences_small_3_%j.out
+#SBATCH --err=output/prediction_both_step_1_sequences_small_3_%j.err
 
 
 module load python/3.11
@@ -16,9 +16,9 @@ cd /home/schen123/scratch/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../sbc806/RumHKNet/src/
+cd ../sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1_v3_fir.sh 0.2 02 newadd_155098MAGs_small_0 1500 50 _v3
+./prediction_scripts/step_1/both/prediction_both_step_1_v3_fir.sh 0.2 02 newadd_155098MAGs_small_3 1500 50 _v3
 
 
 deactivate

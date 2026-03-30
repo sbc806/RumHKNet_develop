0c0bf1c64cd1bb6ec3a746326f1bab7939b95cde
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-30 16:40:20 -0700

    Update run_prediction_both_step_1_newadd_small_0.sh

--- bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_newadd_small_0.sh
+++ bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_newadd_small_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-guanuofa
 #SBATCH --gpus-per-node=h100:1
-#SBATCH --mem=30G
+#SBATCH --mem=32G
 #SBATCH --time=3-0
 #SBATCH --job-name=prediction-both-step-1-sequences-small-0
 #SBATCH --output=output/prediction_both_step_1_sequences_small_0_%j.out
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1_v3_fir.sh 0.3 03 clustered_rep_seq95_small_10 1500 50 _v3
+./prediction_scripts/step_1/both/prediction_both_step_1_v3_fir.sh 0.2 02 clustered_rep_seq95_small_10 1500 50 _v3
 
 
 deactivate

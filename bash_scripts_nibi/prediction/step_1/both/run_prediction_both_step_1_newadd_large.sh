610abd288dd4be0aea54b9b6289e2c735220f2ad
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-03-30 15:16:09 -0700

    Update run_prediction_both_step_1_newadd_large.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_newadd_large.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_newadd_large.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
-#SBATCH --time=3:0:0
-#SBATCH --job-name=prediction-both-step-1-sequences-large-sorted-largest
-#SBATCH --output=output/prediction_both_step_1_sequences_large_sorted_largest_%j.out
-#SBATCH --err=output/prediction_both_step_1_sequences_large_sorted_largest_%j.err
+#SBATCH --time=3-0
+#SBATCH --job-name=prediction-both-step-1-sequences-large
+#SBATCH --output=output/prediction_both_step_1_sequences_large_%j.out
+#SBATCH --err=output/prediction_both_step_1_sequences_large_%j.err
 
 
 module load python/3.11

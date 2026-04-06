abfc14d9fd1f63ac5b1362e262218ad6451ebf56
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-05 17:14:49 -0700

    Update run_prediction_both_step_2_newadd_small.sh

---
+++ bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_newadd_small.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=2-0
+#SBATCH --job-name=prediction-both-step-2-small-0-v3
+#SBATCH --output=output/prediction_both_step_2_small_0_v3_%j.out
+#SBATCH --err=output/prediction_both_step_2_small_0_v3_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+cd /home/schen123/links/projects/def-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet_develop/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1/both/prediction_both_step_2_v3_rorqual.sh 0.3 03 newadd_155098MAGs_small_step_1_kinase 1500 50 _v3
+
+
+deactivate

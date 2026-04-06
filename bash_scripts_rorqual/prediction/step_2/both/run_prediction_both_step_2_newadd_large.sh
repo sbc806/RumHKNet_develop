76110268b201750abe4ddc8da9838ed951935878
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-05 17:16:11 -0700

    Update run_prediction_both_step_2_newadd_large.sh

--- bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_newadd_large.sh
+++ bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_newadd_large.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_2/both/prediction_both_step_2_rorqual.sh 0.2 02 newadd_155098MAGs_large_step_1_kinase 34551 _v2
+./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.2 02 newadd_155098MAGs_large_step_1_kinase 34551 _v2
 
 
 deactivate

f8817fa59c5a361948a37356f84fd470d6e81e37
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-05 17:15:14 -0700

    Update run_prediction_both_step_2_newadd_small.sh

--- bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_newadd_small.sh
+++ bash_scripts_rorqual/prediction/step_2/both/run_prediction_both_step_2_newadd_small.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=2-0
-#SBATCH --job-name=prediction-both-step-2-small-0-v3
-#SBATCH --output=output/prediction_both_step_2_small_0_v3_%j.out
-#SBATCH --err=output/prediction_both_step_2_small_0_v3_%j.err
+#SBATCH --job-name=prediction-both-step-2-small-v3
+#SBATCH --output=output/prediction_both_step_2_small_v3_%j.out
+#SBATCH --err=output/prediction_both_step_2_small_v3_%j.err
 
 
 module load python/3.11

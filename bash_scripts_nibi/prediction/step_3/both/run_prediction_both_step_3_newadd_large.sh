fe72f06b3f64dff0fea0f52f21b91ee553dc1580
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-06 21:55:20 -0700

    Update run_prediction_both_step_3_newadd_large.sh

---
+++ bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_newadd_large.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=1:0:0
+#SBATCH --job-name=prediction-both-step-3-large-v3
+#SBATCH --output=output/prediction_both_step_3_large_v3_%j.out
+#SBATCH --err=output/prediction_both_step_3_large_v3_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_3/both/prediction_both_step_3.sh 0.2 02 newadd_055198MAGs_large_step_2_histidine_kinase 34551
+
+
+deactivate

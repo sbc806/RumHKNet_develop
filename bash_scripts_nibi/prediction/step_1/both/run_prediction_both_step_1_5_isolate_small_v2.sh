584dfbbc135a513251842a216baaf1e2dd028623
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 14:03:11 -0700

    Update run_prediction_both_step_1_5_isolate_small_v2.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_5_isolate_small_v2.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_5_isolate_small_v2.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=32G
 #SBATCH --time=3:0:0
 #SBATCH --job-name=prediction-both-step-1-sequences-small
-#SBATCH --output=output/prediction_both_step_1_sequences_large_%j.out
-#SBATCH --err=output/prediction_both_step_1_sequences_large_%j.err
+#SBATCH --output=output/prediction_both_step_1_sequences_small_%j.out
+#SBATCH --err=output/prediction_both_step_1_sequences_small_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.2 02 5_isolate_large 34551 _v2
+./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.2 02 5_isolate_small 34551 _v2
 
 
 deactivate

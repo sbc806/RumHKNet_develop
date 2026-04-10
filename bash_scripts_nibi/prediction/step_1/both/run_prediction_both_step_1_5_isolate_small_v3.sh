51ad58a8688dc6a19f5eaaa770fbf9711caf778e
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 13:57:07 -0700

    Update run_prediction_both_step_1_5_isolate_small_v3.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_5_isolate_small_v3.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_5_isolate_small_v3.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.2 02 5_isolate_small 1500 50 _v3
+./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.2 02 5_isolate_small 1500 20 _v3
 
 
 deactivate

4d66bde15c8bb0199b7768e568f9fcc991d28025
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-10 14:03:34 -0700

    Update run_prediction_both_step_1_5_isolate_small_v2.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_5_isolate_small_v2.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_5_isolate_small_v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-guanuofa
-#SBATCH --gpus-per-node=nbidia_h100:1
+#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=32G
 #SBATCH --time=3:0:0
 #SBATCH --job-name=prediction-both-step-1-sequences-small

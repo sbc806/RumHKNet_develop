187ac7c21fc8f9958f97e6b5943d75e5f0ee55a5
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:21:00 -0700

    Update run_prediction_both_step_4_5_isolate_small.sh

--- bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_5_isolate_small.sh
+++ bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_5_isolate_small.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_4/both/prediction_both_step_4.sh 0.3 03 clustered_rep_seq95_large_histidine_kinase_batch 34551
+./prediction_scripts/step_4/both/prediction_both_step_4.sh 0.2 02 5_isolate_step_3_histidine_kinase_family_small 34551
 
 
 deactivate

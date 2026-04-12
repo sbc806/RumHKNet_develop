504be0a64f2df12658c9636e7a1e910f449826d6
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 06:20:17 -0700

    Update run_prediction_both_step_4_5_isolate_small.sh

--- bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_5_isolate_small.sh
+++ bash_scripts_nibi/prediction/step_4/both/run_prediction_both_step_4_5_isolate_small.sh
@@ -16,7 +16,7 @@ cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
+cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
 ./prediction_scripts/step_4/both/prediction_both_step_4.sh 0.3 03 clustered_rep_seq95_large_histidine_kinase_batch 34551
 

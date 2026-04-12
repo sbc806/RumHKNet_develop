ce0c8a9e9d273df8027c707ad2ea2994fd3cd8d0
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-12 01:04:15 -0700

    Update run_prediction_both_step_3_5_isolate_small.sh

--- bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_5_isolate_small.sh
+++ bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_5_isolate_small.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=rrg-guanuofa
-#SBATCH --gpus-per-node=h100:1
+#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=32G
-#SBATCH --time=12:0:0
-#SBATCH --job-name=prediction-both-step-3-large-v3
-#SBATCH --output=output/prediction_both_step_3_large_v3_%j.out
-#SBATCH --err=output/prediction_both_step_3_large_v3_%j.err
+#SBATCH --time=0:5:0
+#SBATCH --job-name=prediction-both-step-3-small-v3
+#SBATCH --output=output/prediction_both_step_3_small_v3_%j.out
+#SBATCH --err=output/prediction_both_step_3_small_v3_%j.err
 
 
 module load python/3.11

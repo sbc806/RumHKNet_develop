4f7210fc6448b5779e5c7f29b39a28282e90e510
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-22 13:39:06 -0700

    Update run_prediction_both_step_1_2026_04_22_large.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_2026_04_22_large.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_2026_04_22_large.sh
@@ -16,7 +16,7 @@ cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../sbc806/RumHKNet/src/
+cd /home/schen123/scratch/kinases/RumHKNet_develop/sbc806/RumHKNet/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
 ./prediction_scripts/step_1/both/prediction_both_step_1.sh 0.2 02 2026_04_22_clustered95_rep_seq_large 34551 _v2
 

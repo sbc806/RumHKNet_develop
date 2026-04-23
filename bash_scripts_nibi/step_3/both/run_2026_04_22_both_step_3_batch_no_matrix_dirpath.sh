e9afe1220d76e5cd1ffe6dc33a8b6a446598009a
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-22 23:24:11 -0700

    Update run_2026_04_22_both_step_3_batch_no_matrix_dirpath.sh

--- bash_scripts_nibi/step_3/both/run_2026_04_22_both_step_3_batch_no_matrix_dirpath.sh
+++ bash_scripts_nibi/step_3/both/run_2026_04_22_both_step_3_batch_no_matrix_dirpath.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=def-guanuofa
-#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus=h100:1
 #SBATCH --mem=100G
-#SBATCH --time=7-0
-#SBATCH --job-name=both-step-3-batch-no-matrix-dirpath
-#SBATCH --output=output/both_step_3_batch_no_matrix_dirpath_%j.out
-#SBATCH --err=output/both_step_3_batch_no_matrix_dirpath_%j.err
+#SBATCH --time=2:0:0
+#SBATCH --job-name=2026-04-22-both-step-3-batch-no-matrix-dirpath
+#SBATCH --output=output/2026_04_22_both_step_3_batch_no_matrix_dirpath_%j.out
+#SBATCH --err=output/2026_04_22_both_step_3_batch_no_matrix_dirpath_%j.err
 
 
 module load python/3.11
@@ -17,9 +17,9 @@ cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd ../sbc806/RumHKNet/src/training/V3
-cat both_step_3_batch_no_matrix_dirpath.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_3/both/output/both_step_3_batch_no_matrix_dirpath_$SLURM_JOB_ID.txt
-./both_step_3_batch_no_matrix_dirpath.sh
+cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/training/V3
+cat 2026_04_22_both_step_3_batch_no_matrix_dirpath.sh > /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/bash_scripts_nibi/step_3/both/output/2026_04_22_both_step_3_batch_no_matrix_dirpath_$SLURM_JOB_ID.txt
+./2026_04_22_both_step_3_batch_no_matrix_dirpath.sh
 
 
 deactivate

ec3da8b2c0b84171738b21307195755db8847faa
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-25 23:06:49 -0700

    Update run_prediction_both_step_2_2026_04_22_small_2.sh

---
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_2026_04_22_small_2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=rrg-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=32G
+#SBATCH --time=1-0
+#SBATCH --job-name=prediction-both-step-2-2026-04-22-small-v3
+#SBATCH --output=output/prediction_both_step_2_2026_04_22_small_v3_%j.out
+#SBATCH --err=output/prediction_both_step_2_2026_04_22_small_v3_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/21.0.0
+
+cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_2/both/prediction_both_step_2_v3.sh 0.2 02 2026_04_22_clustered95_rep_seq_step_1_kinase_small 1500 50 _v3
+
+
+deactivate
+
+

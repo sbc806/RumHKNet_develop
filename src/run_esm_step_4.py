5cdfdb4b38cff59b59be1535a79bbc0d03c60110
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-27 02:33:12 -0700

    Update run_esm_step_4.py

--- src/run_esm_step_4.py
+++ src/run_esm_step_4.py
@@ -933,7 +933,7 @@ def main():
         log_fp.write("checkpoint path: %s\n" % checkpoint)
         # model = model_class.from_pretrained(checkpoint, args=args)
         model_trained_dir_path = "/home/schen123/scratch/kinases/sbc806/RumHKNet_develop/models_esm_step_3_batch"
-        model = model_class.from_pretrained(os.path.join(model_trained_dir_path,"extra_p_133_class_v3_batch/protein/multi_class/lucaprot/seq_matrix/20251209200204/checkpoint-100221"), args=args)
+        model = model_class.from_pretrained(os.path.join(model_trained_dir_path,"extra_p_133_class_v3_batch/protein/multi_class/lucaprot/matrix/20251209200204/checkpoint-100221"), args=args)
         model.to(args.device)
         result = test(args, model, parse_row_func, batch_data_func, prefix=prefix, log_fp=log_fp,filter_131=True)
         result = dict(("evaluation_" + k + "_{}".format(global_step), v) for k, v in result.items())

7cb243ec9bf554b586ab68c5ccece3485710a642
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-04-27 22:44:21 -0700

    Update run_prediction_both_step_3_2026_04_22_large_1.sh

--- bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_2026_04_22_large_1.sh
+++ bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_2026_04_22_large_1.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=0:5:0
-#SBATCH --job-name=prediction-both-step-3-large-v3
-#SBATCH --output=output/prediction_both_step_3_large_v3_%j.out
-#SBATCH --err=output/prediction_both_step_3_large_v3_%j.err
+#SBATCH --job-name=prediction-both-step-3-2026-04-22-large-1-v2
+#SBATCH --output=output/prediction_both_step_3_2026_04_22_large_1_v2_%j.out
+#SBATCH --err=output/prediction_both_step_3_2026_04_22_large_1_v2_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_3/both/prediction_both_step_3.sh 0.2 02 newadd_155098MAGs_large_step_2_histidine_kinase 34551
+./prediction_scripts/step_3/both/prediction_both_step_3.sh 0.2 02 2026_04_22_clustered95_rep_seq_step_2_histidine_kinase_large_1 34551 _v2
 
 
 deactivate

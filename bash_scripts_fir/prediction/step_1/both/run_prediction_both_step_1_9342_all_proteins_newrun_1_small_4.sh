f311ee160775b52bc376c585bb6d5d27d2b5aaa1
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-18 15:31:47 -0700

    Update run_prediction_both_step_1_9342_all_proteins_newrun_1_small_4.sh

---
+++ bash_scripts_fir/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_newrun_1_small_4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-guanuofa
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --mem=64G
+#SBATCH --time=3-0
+#SBATCH --job-name=prediction-both-step-1-9342-all-proteins-newrun-1-small-4
+#SBATCH --output=output/prediction_both_step_1_9342_all_proteins_newrun_1_small_4_%j.out
+#SBATCH --err=output/prediction_both_step_1_9342_all_proteins_newrun_1_small_4_%j.err
+
+
+module load python/3.11
+module load scipy-stack
+module load gcc arrow/19.0.1
+
+cd /home/schen123/scratch/kinases/virtual_environments
+source TEST/bin/activate
+
+
+cd ../sbc806/RumHKNet_develop/src/
+# cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
+./prediction_scripts/step_1/both/prediction_both_step_1_v3_fir.sh 0.2 02 9342_all_proteins_newrun_1_small_4_remaining 1500 50 _v3
+
+
+deactivate

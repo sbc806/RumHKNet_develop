59a2df88dc5daebd83bf802ed90c98e156989e9f
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-18 15:15:56 -0700

    Update run_prediction_both_step_1_9342_all_proteins_newrun_2_small_5.sh

--- bash_scripts_rorqual/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_newrun_2_small_5.sh
+++ bash_scripts_rorqual/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_newrun_2_small_5.sh
@@ -3,9 +3,9 @@
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=32G
 #SBATCH --time=3-0
-#SBATCH --job-name=prediction-both-step-1-9342-all-proteins-newrun-2-small-3-v3
-#SBATCH --output=output/prediction_both_step_1_9342_all_proteins_newrun_2_small_3_v3_%j.out
-#SBATCH --err=output/prediction_both_step_1_9342_all_proteins_newrun_2_small_3_v3_%j.err
+#SBATCH --job-name=prediction-both-step-1-9342-all-proteins-newrun-2-small-5-v3
+#SBATCH --output=output/prediction_both_step_1_9342_all_proteins_newrun_2_small_5_v3_%j.out
+#SBATCH --err=output/prediction_both_step_1_9342_all_proteins_newrun_2_small_5_v3_%j.err
 
 
 module load python/3.11
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd ../sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1_v3_rorqual.sh 0.2 02 9342_all_proteins_newrun_2_small_3 1500 32 _v3
+./prediction_scripts/step_1/both/prediction_both_step_1_v3_rorqual.sh 0.2 02 9342_all_proteins_newrun_2_small_5 1500 32 _v3
 
 
 deactivate

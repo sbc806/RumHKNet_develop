b50f8f9422337bcb6dfcddd201e7d5b7af327d5b
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-21 20:50:28 -0700

    Update run_prediction_both_step_1_9342_all_proteins_newrun_2_small_2.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_newrun_2_small_2.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_newrun_2_small_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=rrg-guanuofa
-#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --gpus-per-node=h100:1
 #SBATCH --mem=32G
 #SBATCH --time=3-0
 #SBATCH --job-name=prediction-both-step-1-9342-all-proteins-newrun-2-small-2-v3
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.2 02 9342_all_proteins_newrun_2_small_2 1500 24 _v3_1
+./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.2 02 9342_all_proteins_newrun_2_small_2 1500 50 _v3again
 
 
 deactivate

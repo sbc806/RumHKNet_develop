297a55027ea7139a9469804367d7a72ac2a2f700
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 21:47:59 -0700

    Update run_prediction_both_step_2_9342_all_proteins_newrun_large_1_2.sh

--- bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_9342_all_proteins_newrun_large_1_2.sh
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_9342_all_proteins_newrun_large_1_2.sh
@@ -18,7 +18,7 @@ source TEST/bin/activate
 
 cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
-./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.2 02 9342_all_proteins_newrun_step_1_kinase_large_1_2 1500 _v2
+./prediction_scripts/step_2/both/prediction_both_step_2.sh 0.2 02 9342_all_proteins_newrun_step_1_kinase_large_1_2 34551 _v2
 
 
 deactivate

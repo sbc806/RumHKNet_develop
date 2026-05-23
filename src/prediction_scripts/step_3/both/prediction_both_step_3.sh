4b5f15b1aa3f95380b668c2b08d43f6a8e6a5965
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 22:24:25 -0700

    Update prediction_both_step_3.sh

--- src/prediction_scripts/step_3/both/prediction_both_step_3.sh
+++ src/prediction_scripts/step_3/both/prediction_both_step_3.sh
@@ -8,7 +8,7 @@ echo $threshold
 python -u prediction_v2.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_3/clustered/$3.csv \
-    --model_path $dir_path_1/sbc806/RumHKNet_develop/models_step_3_both_11_family \
+    --model_path $dir_path_1/sbc806_1/RumHKNet_develop/models_step_3_both_11_family \
     --save_path $dir_path/predictions/predicted_results/step_3/both/clustered/$3_predicted_$2$5.csv \
     --dataset_name step_3_11_family \
     --dataset_type protein \

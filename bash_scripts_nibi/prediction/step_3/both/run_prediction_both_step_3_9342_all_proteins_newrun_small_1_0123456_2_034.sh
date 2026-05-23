9916d0ac9b03108bdc0b0ca95e3df42f7ca6dd24
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-22 22:00:33 -0700

    Update run_prediction_both_step_3_9342_all_proteins_newrun_small_1_0123456_2_034.sh

--- bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_9342_all_proteins_newrun_small_1_0123456_2_034.sh
+++ bash_scripts_nibi/prediction/step_3/both/run_prediction_both_step_3_9342_all_proteins_newrun_small_1_0123456_2_034.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=1-0
 #SBATCH --job-name=prediction-both-step-3-9342-all-proteins-newrun-small-1-0123456-2-034-v3
 #SBATCH --output=output/prediction_both_step_3_9342_all_proteins_newrun_small_1_0123456_2_034_v3_%j.out
-#SBATCH --err=output/prediction_both_step_3_9342_all_proteins_newrun_small-1_0123456_2_034_v3_%j.err
+#SBATCH --err=output/prediction_both_step_3_9342_all_proteins_newrun_small_1_0123456_2_034_v3_%j.err
 
 
 module load python/3.11

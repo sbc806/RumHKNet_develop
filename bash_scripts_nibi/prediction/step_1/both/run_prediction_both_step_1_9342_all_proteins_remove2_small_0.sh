b45e77881d8aa86db83be4df183f466fad236261
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-24 23:33:48 -0700

    Update run_prediction_both_step_1_9342_all_proteins_remove2_small_0.sh

--- bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_remove2_small_0.sh
+++ bash_scripts_nibi/prediction/step_1/both/run_prediction_both_step_1_9342_all_proteins_remove2_small_0.sh
@@ -16,7 +16,7 @@ cd /home/schen123/projects/rrg-guanuofa/schen123/kinases/virtual_environments
 source TEST/bin/activate
 
 
-cd /home/schen123/scratch/kinases/sbc806/RumHKNet_develop/src/
+cd /home/schen123/scratch/kinases/sbc806_1/RumHKNet_develop/src/
 # cat transformer_step_2_4_1.sh > /home/schen123/projects/rrg-guanuofa/schen123/kinases/sbc806/RumHKNet/bash_scripts_nibi/step_2/transformer/output/transformer_step_2_4_1_$SLURM_JOB_ID.txt
 ./prediction_scripts/step_1/both/prediction_both_step_1_v3.sh 0.2 02 9342_all_proteins_remove2_small_0 1500 50 _v3
 

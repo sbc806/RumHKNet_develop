d640fc8d2f99c1babd3efa90b3d0e2e195699965
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-26 18:03:58 -0700

    Update run_prediction_both_step_2_9342_all_proteins_remove2_small_3.sh

--- bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_9342_all_proteins_remove2_small_3.sh
+++ bash_scripts_nibi/prediction/step_2/both/run_prediction_both_step_2_9342_all_proteins_remove2_small_3.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=rrg-guanuofa
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=32G
-#SBATCH --time=6:0:0
+#SBATCH --time=8:0:0
 #SBATCH --job-name=prediction-both-step-2-9342-all-proteins-remove2-small-3
 #SBATCH --output=output/prediction_both_step_2_9342_all_proteins_remove2_small_3_%j.out
 #SBATCH --err=output/prediction_both_step_2_9342_all_proteins_remove2_small_3_%j.err

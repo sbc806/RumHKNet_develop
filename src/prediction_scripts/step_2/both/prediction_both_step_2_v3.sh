45bb9af9ffd89a307095e29ba124e24bad5f2e26
sbc806 <76720208+sbc806@users.noreply.github.com>
2026-05-26 18:00:19 -0700

    Update prediction_both_step_2_v3.sh

--- src/prediction_scripts/step_2/both/prediction_both_step_2_v3.sh
+++ src/prediction_scripts/step_2/both/prediction_both_step_2_v3.sh
@@ -15,7 +15,7 @@ echo $extra
 python -u prediction_v3.py \
     --seq_type prot \
     --input_file $dir_path/predictions/predictions_dataset/step_2/clustered/$3.csv \
-    --model_path $dir_path_1/sbc806/RumHKNet_develop/models/step_2/both \
+    --model_path $dir_path_1/sbc806_1/RumHKNet_develop/models/step_2/both \
     --save_path $dir_path/predictions/predicted_results/step_2/both/clustered \
     --dataset_name extra_p_2_class_v3_kinases_only \
     --dataset_type protein \
